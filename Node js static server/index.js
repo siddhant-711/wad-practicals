1e76f425e01385d127ba9c1b5142882efca5f358
siddhant-711 <112260375+siddhant-2002@users.noreply.github.com>
2024-05-03 01:09:25 +0530

    new

---
+++ Node js static server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.use("/", (req, res) => res.send("server is working fine"));
+
+app.use("/home",(req,res) => {res.send("Welcome to home page")});
+
+app.listen(PORT, () =>
+  console.log(`local server is running on: http://localhost:${PORT}`)
+);
+
